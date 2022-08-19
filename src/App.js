618b23c64416096464a4b0035cdad92bf93008a3
Anna Crandall <anna.crandall02@gmail.com>
2022-08-19 12:49:11 -0500

    added comments / deleted extra files

--- src/App.js
+++ src/App.js
@@ -10,31 +10,55 @@ import drumsNine from "./drum-9.wav";
 
 const drumKit = [
   { name: "tamborine", audio: drumsOne, keyPress: "Q" },
-  { name: "hydroflask", audio: drumsTwo, keyPress: "W" },
-  { name: "house", audio: drumsThree, keyPress: "E" },
-  { name: "yeah", audio: drumsFour, keyPress: "A" },
-  { name: "wall", audio: drumsFive, keyPress: "S" },
-  { name: "pool", audio: drumsSix, keyPress: "D" },
-  { name: "allgreen", audio: drumsSeven, keyPress: "Z" },
-  { name: "soylent", audio: drumsEight, keyPress: "X" },
-  { name: "kale", audio: drumsNine, keyPress: "C" },
+  { name: "horror-movie", audio: drumsTwo, keyPress: "W" },
+  { name: "achievement-sound", audio: drumsThree, keyPress: "E" },
+  { name: "rolling-drum", audio: drumsFour, keyPress: "A" },
+  { name: "wacky-sound", audio: drumsFive, keyPress: "S" },
+  { name: "more-drums", audio: drumsSix, keyPress: "D" },
+  { name: "organ", audio: drumsSeven, keyPress: "Z" },
+  { name: "horror-drums", audio: drumsEight, keyPress: "X" },
+  { name: "", audio: drumsNine, keyPress: "C" },
 ];
 function App() {
   const handleClick = (audio) => {
-    console.log(audio);
     const playAudio = new Audio(audio);
     playAudio.play();
+     
+     // const HandleKeyDown = (keyPress) ? => {
+    // if (certain key is pressed, trigger corresponding drum){
+    //    "Q" === obj of {keyPress: "Q"}; 
+    //    "W" === obj of {keyPress: "W"};
+    //    "E" === obj of {keyPress: "D"};
+    //    "A" === obj of {keyPress: "A"};
+    //    "S" === obj of {keyPress: "S"};
+    //    "D" === obj of {keyPress: "D"};
+    //    "Z" === obj of {keyPress: "Z"}; 
+    //    "X" === obj of {keyPress: "X"};
+    //    "C" === obj of {keyPress: "C"};
+    // }
+   // }
+    
+    //     
+    //   (how to add two events to one button)
+   // }
+   // }
+  
   };
   return (
+    <section className="border-4 border-black m-10 p-10 rounded">
+      <div className="border-2 border-black rounded drop-shadow">
+        <h1 className="text-center text-xl font-mono tracking-wide">Ana's Drum Kit</h1>
+        </div>
     <div className="relative" id="drum-machine">
       <div className="flex justify-center gap-4 items-center h-screen" id="display">
         {drumKit.map(({ keyPress, audio, name }) => (
           <button
             className="drum-pad border-2 border-black p-2 rounded hover:bg-slate-600 hover:text-white transition-all 
-            hover:shadow-lg hover:shadow-violet-500"
+            hover:shadow-lg hover:shadow-lime-800 "
             key={keyPress}
             id={name}
             onClick={() => handleClick(audio)}
+            //onKeyDown = {() => handleClick(audio)} OR new const? 
             type="button"
           >
             {keyPress}
@@ -42,7 +66,8 @@ function App() {
           </button>
         ))}
       </div>
-    </div>
+      </div>
+      </section>
   );
 }
 
